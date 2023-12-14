ec85b59c3fb136702c0767455af3204ac2db9f67
Salvatore <salvatoretilocca860@gmail.com>
2023-12-14 18:42:04 +0100

    try to fix pil loader without cv2

--- dataset/cityscapes.py
+++ dataset/cityscapes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 from torch.utils.data import Dataset
-from PIL import Image
+from PIL import Image,ImageFile
 from utils import one_hot_it_v11,RandomCrop 
 import os
 import os.path
@@ -14,18 +14,15 @@ import numpy as np
 import json
 import random
 import cv2
-
-def cv2_loader_label(path):
-    # Carica l'immagine in scala di grigi
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    return img
-
-def cv2_loader(path):
-    # Carica l'immagine a colori
-    img = cv2.imread(path)
-    # Converti da BGR a RGB
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img
+ImageFile.LOAD_TRUNCATED_IMAGES=True
+def pil_loader_label(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+    return img.convert('L')
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+    return img.convert('RGB')
 class CityScapes(Dataset):
     
     def __init__(self, mode,root, cropsize=(640, 480),randomscale=(0.125, 0.25, 0.375, 0.5, 0.675, 0.75, 0.875, 1.0, 1.25, 1.5)):
@@ -43,10 +40,9 @@ class CityScapes(Dataset):
                          transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                          ])
         self.to_tensor_label = transforms.Compose([
-                            # other transforms
-                             transforms.ToTensor(),
-                             transforms.Normalize(mean=(0,)*1, std=(255,)*1)
-                                ])
+                    transforms.PILToTensor() 
+                ])
+       
         
         for city in os.listdir(image_dir):
             folder_path = os.path.join(image_dir, city)
@@ -71,10 +67,10 @@ class CityScapes(Dataset):
     def __getitem__(self, idx):
         image_path = self.data["image_path"].iloc[idx]
         label_path=self.data["label_path"].iloc[idx]
-        image,label = cv2_loader(image_path),cv2_loader_label(label_path)
+        image,label = pil_loader(image_path),pil_loader_label(label_path)
 
-        image=self.to_tensor(image)
-        label=self.to_tensor_label(label)
+        image=self.to_tensor(image).float()
+        label=self.to_tensor_label(label).float()
 
         return image, label
     
