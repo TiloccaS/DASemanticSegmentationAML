b591d7aed80aaa50025bd72ca8dfac6e00f152ec
Salvatore <salvatoretilocca860@gmail.com>
2023-12-21 10:01:00 +0100

    try to fix bug

--- dataset/cityscapes.py
+++ dataset/cityscapes.py
@@ -18,7 +18,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES=True
 def pil_loader_label(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
-        return img.convert('L')
+        return img
 def pil_loader(path):
     with open(path, 'rb') as f:
         img = Image.open(f)
